750bed74b223f8616c4f77dbe8afb5277bc27da0
Яроцкий Алексей Влдаимирович <aleksey.yarozkiy@yandex.ru>
2022-07-05 12:47:13 +0200

    Upload project

---
+++ src/main/java/ru/netology/main/Movie.java
@@ -0,0 +1,16 @@
+package ru.netology.main;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Movie {
+    private int id;
+    private String name;
+    private String movieGenre;
+    private boolean premiereTomorrow;
+
+}
